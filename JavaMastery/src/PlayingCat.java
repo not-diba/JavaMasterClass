43d72a4a542e93005ed9e0b57ab2fec5a3de26a4
not-diba <elvis.madiba@gmail.com>
2025-12-04 07:31:35 +0300

    feat: playing cat

---
+++ JavaMastery/src/PlayingCat.java
@@ -0,0 +1,16 @@
+public class PlayingCat {
+  public static void main(String[] args) {
+    System.out.println(isCatPlaying(true, 10)); // true
+    System.out.println(isCatPlaying(false, 36)); // false
+    System.out.println(isCatPlaying(false, 35)); // true
+  }
+
+  public static boolean isCatPlaying(boolean summer, int temperature) {
+    int upperLimit = summer ? 45 : 35;
+    if (temperature >= 25 && temperature <= upperLimit) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+}
