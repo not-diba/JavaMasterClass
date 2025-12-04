f9c75ff25b65f6c0c6828c8134293a196e058129
not-diba <elvis.madiba@gmail.com>
2025-12-04 07:31:19 +0300

    feat: Minutes to years

---
+++ JavaMastery/src/MinutesToYearsDaysCalculator.java
@@ -0,0 +1,20 @@
+public class MinutesToYearsDaysCalculator {
+  public static void main(String[] args) {
+    printYearsAndDays(525600);
+    printYearsAndDays(1051200);
+    printYearsAndDays(561600);
+  }
+
+  public static void printYearsAndDays(long minutes) {
+    if (minutes < 0) {
+      System.out.println("Invalid Value");
+      return;
+    }
+
+    long years = minutes / 525600;
+    long remainingMinutes = minutes % 525600;
+    long days = remainingMinutes / 1440;
+
+    System.out.println(minutes + " min = " + years + " y and " + days + " d");
+  }
+}
